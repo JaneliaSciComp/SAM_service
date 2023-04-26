f6c7d250f97faae9e24dbe19304b37beeab33c98
Jody Clements <jody@neomorphic.co.uk>
2023-04-26 09:55:45 -0400

    Adds logging info to prediction route

--- sam_service/sam_service.py
+++ sam_service/sam_service.py
@@ -1,3 +1,4 @@
+import logging
 from io import BytesIO
 from flask import Flask, render_template, request, redirect, flash, send_file
 import cv2
@@ -5,12 +6,17 @@ import numpy as np
 
 import paintera_sam.sam_model as model
 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
 app = Flask(__name__)
 
 app.secret_key = "cdas69786cdsa^%#FDS^%$gfd65#$dfs#@$#@?><i:OI)(*-"
 
-sam_predictor = model.new_predictor(device='cpu')
+logging.info('Creating new predictor...')
 
+sam_predictor = model.new_predictor(device='cuda')
+
+logging.info('Created new predictor')
 
 def predict(cv_image, coords):
     print("running prediction")
@@ -29,6 +35,7 @@ def service_docs():
 @app.route("/prediction", methods=["POST"])
 def prediction():
     """accepts an input image and coordinates and returns a prediction"""
+    logging.info('Started prediction route ...')
     # get the image from the request object
     # check if the post request has the file part
     if "image" not in request.files:
@@ -40,6 +47,7 @@ def prediction():
 
     nparr = np.frombuffer(file_data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    logging.info('image loaded from POST input ...')
 
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
@@ -56,14 +64,16 @@ def prediction():
         flash("No y coordinate specified")
         return redirect("/")
     y_coordinate = request.form.get("y")
+    logging.info('coordinates loaded from POST input ...')
 
     # pass everything to the prediction method
     mask_image = predict(img, [x_coordinate, y_coordinate])
-    print(mask_image)
+    logging.info('mask returned from predictor ...')
 
     # return the mask.
     # file_stream = BytesIO(file_data)
     file_stream = BytesIO(mask_image)
+    logging.info('file_stream ready to send ...')
     return send_file(file_stream, mimetype="image/png")
 
 
