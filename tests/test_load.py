a05adee221a27525e3e0b0240cede29fcc5061f9
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-02-03 13:29:25 -0500

    added load tester

---
+++ tests/test_load.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import argparse
+import pathlib
+import requests
+import pandas as pd
+from multiprocessing import Pool
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Load testing')
+
+    parser.add_argument('-u', '--baseUrl', dest='base_url', type=str, required=True, \
+        help='Base URL to the SAM service')
+
+    parser.add_argument('-w', '--workers', dest='num_workers', type=int, required=True, default=1, \
+        help='Number of concurrent workers that hit the API')
+
+    parser.add_argument('-r', '--requests', dest='num_requests', type=int, default=10, \
+        help='Number of requests that each worker should send')
+
+    parser.add_argument('-i', '--input', dest='input_image', type=str, default="em1.png", \
+        help='Image file used as test input')
+    
+    parser.add_argument('--describe', action=argparse.BooleanOptionalAction, \
+        help="Print statistics using Pandas describe() format instead of tab-delimited")
+
+    args = parser.parse_args()
+    input = pathlib.Path(args.input_image)
+
+    def test_SAM(worker_id):
+        times = []
+        for request_id in range(args.num_requests):
+            url = f"{args.base_url}/embedded_model"
+            files = {'image': (input.name, open(input.absolute(), 'rb'), 'image/png', {'Expires': '0'})}
+            r = requests.post(url, files=files)
+            assert r.status_code==200
+            times.append(r.elapsed.total_seconds())
+        return times
+
+    with Pool(args.num_workers) as p:
+        series = []
+        elapsed_times = p.map(test_SAM, range(args.num_workers))
+        for times in elapsed_times:
+            t = pd.Series(times)
+            series.append(t)
+
+        all = pd.concat(series)
+
+        if args.display:
+            print(all.describe())
+        else:
+            print("Workers\tRequests\tMean\tStd\tMin\tP25\tP50\tP75\tMax")
+            print(f"{args.num_workers}\t{args.num_requests}\t{all.mean()}\t{all.std()}\t{all.min()}\t{all.quantile(0.25)}\t{all.quantile(0.5)}\t{all.quantile(0.75)}\t{all.max()}")
