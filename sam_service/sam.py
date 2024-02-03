fb98c75f18e88378ba4d53ff542382bff531ac7f
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-02-03 11:27:44 -0500

    moved business logic into separate module

---
+++ sam_service/sam.py
@@ -0,0 +1,160 @@
+import os
+import sys
+import tempfile
+import atexit
+import shutil
+from loguru import logger
+import warnings
+
+from segment_anything import sam_model_registry, SamPredictor
+from segment_anything.utils.onnx import SamOnnxModel
+import paintera_sam.sam_model as model
+import onnxruntime
+from onnxruntime.quantization import QuantType
+from onnxruntime.quantization.quantize import quantize_dynamic
+
+import cv2
+import numpy as np
+import torch
+
+@atexit.register
+def cleanup_temp_dir():
+    try:
+        shutil.rmtree(temp_dir)
+    except IOError:
+        sys.stderr.write('Failed to clean up temp dir {}'.format(temp_dir))
+
+temp_dir = tempfile.mkdtemp()
+
+class SAM:
+
+    def __init__(self, device, model_type, checkpoint):
+        self.device = device
+        self.sam = sam_model_registry[model_type](checkpoint=checkpoint)
+        self.sam.to(device=device)
+
+        logger.info('Creating new predictor...')
+        self.sam_predictor = SamPredictor(self.sam)
+        logger.info('Created new predictor')
+
+        self.ort_session = self.create_onnx_runtime(model_type, checkpoint)
+
+            
+    def create_onnx_runtime(self, model_type, checkpoint):
+        logger.info('Creating new onnx runtime...')
+
+        # there needs to be a seperate onnx_sam model that is sent to the cpu,
+        # because the onnx export occurs on the cpu and not the gpu.
+        onnx_sam = sam_model_registry[model_type](checkpoint=checkpoint)
+        onnx_sam.to(device='cpu')
+
+        onnx_model_path = os.path.join(temp_dir, f"{str(os.getpid())}.sam_onnx.onnx")
+
+        onnx_model = SamOnnxModel(onnx_sam, return_single_mask=True)
+
+        dynamic_axes = {
+            "point_coords": {1: "num_points"},
+            "point_labels": {1: "num_points"},
+        }
+
+        embed_dim = self.sam.prompt_encoder.embed_dim
+        embed_size = self.sam.prompt_encoder.image_embedding_size
+        mask_input_size = [4 * x for x in embed_size]
+        dummy_inputs = {
+            "image_embeddings": torch.randn(1, embed_dim, *embed_size, dtype=torch.float),
+            "point_coords": torch.randint(low=0, high=1024, size=(1, 5, 2), dtype=torch.float),
+            "point_labels": torch.randint(low=0, high=4, size=(1, 5), dtype=torch.float),
+            "mask_input": torch.randn(1, 1, *mask_input_size, dtype=torch.float),
+            "has_mask_input": torch.tensor([1], dtype=torch.float),
+            "orig_im_size": torch.tensor([1500, 2250], dtype=torch.float),
+        }
+        output_names = ["masks", "iou_predictions", "low_res_masks"]
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)
+            warnings.filterwarnings("ignore", category=UserWarning)
+            with open(onnx_model_path, "wb") as f:
+                torch.onnx.export(
+                    onnx_model,
+                    tuple(dummy_inputs.values()),
+                    f,
+                    export_params=True,
+                    verbose=False,
+                    opset_version=17,
+                    do_constant_folding=True,
+                    input_names=list(dummy_inputs.keys()),
+                    output_names=output_names,
+                    dynamic_axes=dynamic_axes,
+                )
+
+        onnx_model_quantized_path = os.path.join(temp_dir, f"{str(os.getpid())}.sam_onnx_quantized_example.onnx")
+
+        quantize_dynamic(
+            model_input=onnx_model_path,
+            model_output=onnx_model_quantized_path,
+            optimize_model=True,
+            per_channel=False,
+            reduce_range=False,
+            weight_type=QuantType.QUInt8,
+        )
+        onnx_model_path = onnx_model_quantized_path
+        logger.info('Created new onnx runtime...')
+
+        return onnxruntime.InferenceSession(onnx_model_path)
+
+
+    def predict(self, cv_image, coords):
+        logger.info(f"running prediction on {self.device}")
+        self.sam_predictor.set_image(cv_image, image_format='BGR')
+        masks = model.predict_current_image(self.sam_predictor, *coords, cv_image, show=False)
+        _, buffer = cv2.imencode('.png', masks[0] * 255)
+        return buffer
+
+
+    def predict_from_embedded(self, image_embedding, coords, img_dimensions):
+        logger.info(f"running embedded prediction on {self.device}")
+        input_point = np.array([coords])
+        input_label = np.array([1])
+        onnx_coord = np.concatenate([input_point, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
+        onnx_label = np.concatenate([input_label, np.array([-1])], axis=0)[None, :].astype(np.float32)
+
+        onnx_coord = self.sam_predictor.transform.apply_coords(onnx_coord, img_dimensions).astype(np.float32)
+
+        onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
+        onnx_has_mask_input = np.zeros(1, dtype=np.float32)
+
+        ort_inputs = {
+            "image_embeddings": image_embedding,
+            "point_coords": onnx_coord,
+            "point_labels": onnx_label,
+            "mask_input": onnx_mask_input,
+            "has_mask_input": onnx_has_mask_input,
+            "orig_im_size": np.array(img_dimensions, dtype=np.float32)
+        }
+
+        masks, _, low_res_logits = self.ort_session.run(None, ort_inputs)
+        masks = masks > self.sam_predictor.model.mask_threshold
+
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+        h, w = masks.shape[-2:]
+        mask_image = masks.reshape(h, w, 1) * 255
+        _, buffer = cv2.imencode('.png', mask_image)
+        return buffer
+
+
+    def get_box_model(self, cv_image):
+        logger.info(f"embedding image on {self.device}")
+        self.sam_predictor.set_image(cv_image, image_format='BGR')
+        return self.sam_predictor.get_image_embedding().cpu().numpy()
+
+
+    def buffer_to_image(self, buffer):
+        nparr = np.frombuffer(buffer, np.uint8)
+        return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+
+    def buffer_to_embedding(self, buffer):
+        nparr = np.frombuffer(buffer, dtype=np.float32)
+        # restore the orginal shape of the numpy array, because frombuffer will
+        # only create a one dimensional array.
+        return nparr.reshape((1,256,64,64))
--- sam_service/sam_fast_api.py
+++ sam_service/sam_fast_api.py
@@ -1,10 +1,5 @@
 import os
-import sys
-import tempfile
-import atexit
-import shutil
 import base64
-import warnings
 import json
 import gzip
 from io import BytesIO
@@ -13,18 +8,9 @@ from typing import Annotated
 from fastapi import FastAPI, File, Form, UploadFile, Response, Query
 from fastapi.responses import StreamingResponse, RedirectResponse, PlainTextResponse
 
-import numpy as np
 import torch
-import cv2
-
-from segment_anything import sam_model_registry, SamPredictor
-from segment_anything.utils.onnx import SamOnnxModel
-import paintera_sam.sam_model as model
-import onnxruntime
-from onnxruntime.quantization import QuantType
-from onnxruntime.quantization.quantize import quantize_dynamic
 from loguru import logger
-
+from sam import SAM
 
 app = FastAPI(
     title="SAM Service",
@@ -34,25 +20,13 @@ app = FastAPI(
     },
 )
 
-
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-logger.info('Creating new predictor...')
+model_type = config.get('MODEL_TYPE', "vit_h")
 module_dir = os.path.dirname(__file__)
-
-model_type = "vit_h"
-checkpoint = os.path.join(module_dir, "sam_vit_h_4b8939.pth")
-
-temp_dir = tempfile.mkdtemp()
-
-@atexit.register
-def cleanup_temp_dir():
-    try:
-        shutil.rmtree(temp_dir)
-    except IOError:
-        sys.stderr.write('Failed to clean up temp dir {}'.format(temp_dir))
-
+checkpoint_file = config.get('CHECKPOINT_FILE', "sam_vit_h_4b8939.pth")
+checkpoint = os.path.join(module_dir, checkpoint_file)
 
 device = 'cpu'
 if torch.cuda.is_available():
@@ -66,123 +40,7 @@ if torch.cuda.is_available():
     if gpu_count > 1:
         device = f'cuda:{str(gpus[os.getpid() % gpu_count])}'
 
-
-sam = sam_model_registry[model_type](checkpoint=checkpoint)
-sam.to(device=device)
-
-sam_predictor = SamPredictor(sam)
-logger.info('Created new predictor')
-
-def create_onnx_runtime():
-    logger.info('Creating new onnx runtime...')
-
-    # there needs to be a seperate onnx_sam model that is sent to the cpu,
-    # because the onnx export occurs on the cpu and not the gpu.
-    onnx_sam = sam_model_registry[model_type](checkpoint=checkpoint)
-    onnx_sam.to(device='cpu')
-
-    onnx_model_path = os.path.join(temp_dir, f"{str(os.getpid())}.sam_onnx.onnx")
-
-    onnx_model = SamOnnxModel(onnx_sam, return_single_mask=True)
-
-    dynamic_axes = {
-        "point_coords": {1: "num_points"},
-        "point_labels": {1: "num_points"},
-    }
-
-    embed_dim = sam.prompt_encoder.embed_dim
-    embed_size = sam.prompt_encoder.image_embedding_size
-    mask_input_size = [4 * x for x in embed_size]
-    dummy_inputs = {
-        "image_embeddings": torch.randn(1, embed_dim, *embed_size, dtype=torch.float),
-        "point_coords": torch.randint(low=0, high=1024, size=(1, 5, 2), dtype=torch.float),
-        "point_labels": torch.randint(low=0, high=4, size=(1, 5), dtype=torch.float),
-        "mask_input": torch.randn(1, 1, *mask_input_size, dtype=torch.float),
-        "has_mask_input": torch.tensor([1], dtype=torch.float),
-        "orig_im_size": torch.tensor([1500, 2250], dtype=torch.float),
-    }
-    output_names = ["masks", "iou_predictions", "low_res_masks"]
-
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)
-        warnings.filterwarnings("ignore", category=UserWarning)
-        with open(onnx_model_path, "wb") as f:
-            torch.onnx.export(
-                onnx_model,
-                tuple(dummy_inputs.values()),
-                f,
-                export_params=True,
-                verbose=False,
-                opset_version=17,
-                do_constant_folding=True,
-                input_names=list(dummy_inputs.keys()),
-                output_names=output_names,
-                dynamic_axes=dynamic_axes,
-            )
-
-    onnx_model_quantized_path = os.path.join(temp_dir, f"{str(os.getpid())}.sam_onnx_quantized_example.onnx")
-
-    quantize_dynamic(
-        model_input=onnx_model_path,
-        model_output=onnx_model_quantized_path,
-        optimize_model=True,
-        per_channel=False,
-        reduce_range=False,
-        weight_type=QuantType.QUInt8,
-    )
-    onnx_model_path = onnx_model_quantized_path
-    logger.info('Created new onnx runtime...')
-
-    return onnxruntime.InferenceSession(onnx_model_path)
-
-
-ort_session = create_onnx_runtime()
-
-
-
-def predict(cv_image, coords):
-    logger.info(f"running prediction on {device}")
-    sam_predictor.set_image(cv_image, image_format='BGR')
-    masks = model.predict_current_image(sam_predictor, *coords, cv_image, show=False)
-    _, buffer = cv2.imencode('.png', masks[0] * 255)
-    return buffer
-
-
-def get_box_model(cv_image):
-    logger.info(f"embedding image on {device}")
-    sam_predictor.set_image(cv_image, image_format='BGR')
-    return sam_predictor.get_image_embedding().cpu().numpy()
-
-
-def predict_from_embedded(image_embedding, coords, img_dimensions):
-    logger.info(f"running embedded prediction on {device}")
-    input_point = np.array([coords])
-    input_label = np.array([1])
-    onnx_coord = np.concatenate([input_point, np.array([[0.0, 0.0]])], axis=0)[None, :, :]
-    onnx_label = np.concatenate([input_label, np.array([-1])], axis=0)[None, :].astype(np.float32)
-
-    onnx_coord = sam_predictor.transform.apply_coords(onnx_coord, img_dimensions).astype(np.float32)
-
-    onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
-    onnx_has_mask_input = np.zeros(1, dtype=np.float32)
-
-    ort_inputs = {
-        "image_embeddings": image_embedding,
-        "point_coords": onnx_coord,
-        "point_labels": onnx_label,
-        "mask_input": onnx_mask_input,
-        "has_mask_input": onnx_has_mask_input,
-        "orig_im_size": np.array(img_dimensions, dtype=np.float32)
-    }
-
-    masks, _, low_res_logits = ort_session.run(None, ort_inputs)
-    masks = masks > sam_predictor.model.mask_threshold
-
-    color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = masks.shape[-2:]
-    mask_image = masks.reshape(h, w, 1) * 255
-    _, buffer = cv2.imencode('.png', mask_image)
-    return buffer
+sam = SAM(device, model_type, checkpoint)
 
 
 @app.get("/", include_in_schema=False)
@@ -202,15 +60,12 @@ async def from_embedded_model(
     logger.info('Started from model route ...')
     file_data = await model.read()
     arr_bytes = base64.b64decode(file_data)
-    nparr = np.frombuffer(arr_bytes, dtype=np.float32)
-    # restore the orginal shape of the numpy array, because frombuffer will
-    # only create a one dimensional array.
-    reshaped = nparr.reshape((1,256,64,64))
+    embedding = sam.buffer_to_embedding(arr_bytes)
     logger.info('embedded image loaded from POST input ...')
 
     # pass everything to the prediction method
-    mask_image = predict_from_embedded(
-            reshaped,
+    mask_image = sam.predict_from_embedded(
+            embedding,
             [x, y],
             [img_y,img_x]
     )
@@ -230,12 +85,11 @@ async def embedded_model(
     """accepts an input image and returns a segement_anything box model"""
     logger.info('Started box_model route ...')
     file_data = await image.read()
-    nparr = np.frombuffer(file_data, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = sam.buffer_to_image(file_data)
     logger.info('image loaded from POST input ...')
 
     # pass everything to the prediction method
-    box_model = get_box_model(img)
+    box_model = sam.get_box_model(img)
     logger.info('model generated ...')
 
     # return the model as base64 string.
@@ -266,9 +120,8 @@ async def predict_form(
     logger.info('Started prediction route ...')
     file_data = await image.read()
     logger.info('image loaded from POST input ...')
-    nparr = np.frombuffer(file_data, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    mask_image = predict(img, [x, y])
+    img = sam.buffer_to_image(file_data)
+    mask_image = sam.predict(img, [x, y])
     logger.info('mask returned from predictor ...')
     file_stream = BytesIO(mask_image)
     logger.info('file_stream ready to send ...')
