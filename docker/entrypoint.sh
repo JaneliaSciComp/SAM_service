59d5ccceb049f5b5bc054d3bde405996c89af62b
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-02-16 14:11:01 -0500

    docker deployment

---
+++ docker/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /opt/conda/etc/profile.d/conda.sh
+conda activate segment_anything
+cd /opt/SAM_service/sam_service || exit
+uvicorn sam_queue:app --access-log --workers 1 --forwarded-allow-ips='*' --proxy-headers --host 0.0.0.0 "$@"
+
